a05b7cdd614836716934a61d7421356e15e90b25
powerar <arpower@bell.net>
2021-07-12 13:56:01 -0400

    added global state

---
+++ client/src/__tests__/reducers.test.js
@@ -0,0 +1,35 @@
+// import our actions
+import {
+    UPDATE_PRODUCTS,
+    UPDATE_CATEGORIES,
+    UPDATE_CURRENT_CATEGORY
+} from '../utils/actions';
+import { reducer } from '../utils/reducers';
+
+//create a sample of what global state will look lke
+const initialState = {
+    products: [],
+    categories: [{ name: 'Food' }],
+    currentCategory: '1',
+};
+
+// test UPDATE_PRODUCTS to see if we can add a product to the products array
+test('UPDATE_PRODUCTS', () => {
+    let newState = reducer(initialState, {
+        type: UPDATE_PRODUCTS,
+        products:[{}, {}]
+    });
+
+    expect(newState.products.length).toBe(2);
+    expect(initialState.products.length).toBe(0);
+});
+
+test('UPDATE_CATEGORIES', () => {
+    let newState = reducer(initialState, {
+        type: UPDATE_CATEGORIES,
+        categories: [{}, {}]
+    });
+
+    expect(newState.categories.length).toBe(2);
+    expect(initialState.categories.length).toBe(1);
+});
---
+++ client/src/utils/actions.js
@@ -0,0 +1,6 @@
+//used by the ProductList component
+export const UPDATE_PRODUCTS = "UPDATE_PRODUCTS";
+//takes the list of categories retrieved and stores it in this global state
+export const UPDATE_CATEGORIES = "UPDATE_CATEGORIES";
+// connecting the data for the above two actions
+export const UPDATE_CURRENT_CATEGORY = "UPDATE_CURRENT_CATEGORY";
