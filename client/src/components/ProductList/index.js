60d57c5eb8936ef2e3fd74e0ade319789b4cf0a4
powerar <arpower@bell.net>
2021-07-13 13:01:31 -0400

    added indexeddb and offline capabilities

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -6,29 +6,47 @@ import { useStoreContext } from '../../utils/GlobalState';
 import { UPDATE_PRODUCTS } from '../../utils/actions';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import spinner from '../../assets/spinner.gif';
+import { idbPromise } from '../../utils/helpers';
 
 function ProductList() {
   const [state, dispatch] = useStoreContext();
 
   const { currentCategory } = state;
-  
+
   const { loading, data } = useQuery(QUERY_PRODUCTS);
-  
+
   useEffect(() => {
+    // if there's data to be stored
     if (data) {
+      // store it in the global state object
       dispatch({
         type: UPDATE_PRODUCTS,
-        products: data.products
+        products: data.products,
+      });
+
+      //also take each product and save it to IndexedDB using the helper function
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
+      //if no connection can be made to graphQL, use the data stored in IndexedDB instead
+    } else if (!loading) {
+      idbPromise('products', 'get').then((products) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: products,
+        });
       });
     }
-  }, [data, dispatch]);
-  
+  }, [data, loading, dispatch]);
+
   function filterProducts() {
     if (!currentCategory) {
       return state.products;
     }
-  
-    return state.products.filter(product => product.category._id === currentCategory);
+
+    return state.products.filter(
+      (product) => product.category._id === currentCategory
+    );
   }
 
   return (
