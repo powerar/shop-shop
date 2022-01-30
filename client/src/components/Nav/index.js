6d16d725fd22cdfcb7853cd685f86d68f2036c36
powerar <arpower@bell.net>
2022-01-30 10:23:06 -0500

    new navbar color and logo location

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,19 +1,16 @@
-import React from "react";
-import Auth from "../../utils/auth";
-import { Link } from "react-router-dom";
+import React from 'react';
+import Auth from '../../utils/auth';
+import { Link } from 'react-router-dom';
 
 function Nav() {
-
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
-        <ul className="flex-row">
-          <li className="mx-1">
-            <Link to="/orderHistory">
-              Order History
-            </Link>
+        <ul className="left">
+          <li>
+            <Link to="/orderHistory">Order History</Link>
           </li>
-          <li className="mx-1">
+          <li>
             {/* this is not using the Link component to logout or user and then refresh the application to the start */}
             <a href="/" onClick={() => Auth.logout()}>
               Logout
@@ -23,16 +20,12 @@ function Nav() {
       );
     } else {
       return (
-        <ul className="flex-row">
-          <li className="mx-1">
-            <Link to="/signup">
-              Signup
-            </Link>
+        <ul>
+          <li>
+            <Link to="/signup">Signup</Link>
           </li>
-          <li className="mx-1">
-            <Link to="/login">
-              Login
-            </Link>
+          <li>
+            <Link to="/login">Login</Link>
           </li>
         </ul>
       );
@@ -40,18 +33,20 @@ function Nav() {
   }
 
   return (
-    <header className="flex-row px-1">
-      <h1>
-        <Link to="/">
-          <span role="img" aria-label="shopping bag">🛍️</span>
-          -Shop-Shop
-        </Link>
-      </h1>
+    <nav className="#0d47a1 blue darken-4">
+      <div className="nav-wrapper">
+        <a className="brand-logo center">
+          <Link to="/">
+            <span role="img" aria-label="shopping bag">
+              🛍️
+            </span>
+            -Shop-Shop
+          </Link>
+        </a>
 
-      <nav>
         {showNavigation()}
-      </nav>
-    </header>
+      </div>
+    </nav>
   );
 }
 
